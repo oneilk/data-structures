1823e203284307e7dde4e44932207a1cfd792ced
oneilk <kwangwanhos@gmail.com>
2020-04-29 16:15:08 -0700

    Update SinglyLinkedList.js

--- js/SinglyLinkedList.js
+++ js/SinglyLinkedList.js
@@ -9,6 +9,7 @@ function SinglyLinkedList () {
 }
 
 SinglyLinkedList.prototype.push = function (data) {
+
 	let node = new Node(data);
 	let currentNode = this.head;
 
@@ -29,4 +30,53 @@ SinglyLinkedList.prototype.push = function (data) {
 	this._length++;
 
 	return node;
+}
+
+SinglyLinkedList.prototype.pop = function (data) {
+
+	let nodeToRemove = this.head;
+
+	if (nodeToRemove != null) {
+		if (this._length == 1) {
+			this.head = null;
+		} else if (this._length == 2) {
+			nodeToRemove = this.head.next;
+			this.head.next = null;
+		} else {
+			let prevNode = nodeToRemove;
+			while (nodeToRemove.next != null) {
+				prevNode = nodeToRemove;
+				nodeToRemove = nodeToRemove.next;
+			}
+			prevNode.next = null;
+		}
+		this._length--;
+	}
+	return nodeToRemove;
+}
+
+SinglyLinkedList.prototype.shift = function (data) {
+	let nodeToRemove = this.head;
+	if (nodeToRemove != null) {
+		this._length--;
+		this.head = this.head.next;
+	}
+	return nodeToRemove;
+}
+
+SinglyLinkedList.prototype.unshift = function (data) {
+
+	let oldHead = this.head;
+	let newHead = new Node(data);
+
+	if (oldHead == null) {
+		this.head = newHead;
+	} else {
+		this.head = newHead;
+		this.head.next = oldHead;
+	}
+
+	this._length++;
+
+	return newHead;
 }
